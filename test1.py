d9215b01387668a8744940f0bc48ab141d6df326
JTK <jtkim@pbees.net>
2024-04-09 17:08:46 +0900

    streamlit

--- test1.py
+++ test1.py
@@ -1,1 +1,14 @@
-print("1")
+import streamlit as st
+import pandas as pd
+
+print("dd")
+
+# Title of the webpage
+st.title('My Simple Streamlit App')
+
+# Write a dataframe
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
