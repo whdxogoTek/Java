e70f0aacde65d011d7813507f7bcfcde90aeb678
JTK <jtkim@pbees.net>
2024-04-09 17:35:06 +0900

    >>>>>수정

--- test1.py
+++ test1.py
@@ -1,19 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-<<<<<<< HEAD
-print("dd")
 
-# Title of the webpage
-st.title('My Simple Streamlit App')
 
-# Write a dataframe
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
-=======
 # Title of the webpage
 st.title('My Interactive Streamlit App')
 
@@ -38,4 +27,4 @@ if st.button('Greet'):
 if st.checkbox('Show DataFrame'):
     st.subheader('DataFrame')
     st.write(dataframe)
->>>>>>> streamlit_code
+
